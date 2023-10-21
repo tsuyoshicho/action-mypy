da9ca73eb631a668802c3f8097aa65ffd31045ef
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2023-10-21 19:01:17 +0900

    test: update test in testerror

--- testerror/success.py
+++ testerror/success.py
@@ -1,3 +1,10 @@
+import requests
+
 def mul(a: int, b: int) -> int:
     return a * b
+
+def test_requests() -> None:
+    url = 'https://httpbin.org/get'
+    r = requests.get(url)
+    print(r.text)
 
